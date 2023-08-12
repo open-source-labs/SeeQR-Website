6baf6845588fb034b04c89542658b271fdd1952f
ChaseSizemore <ChaseLSizemore@gmail.com>
2023-08-12 14:35:20 -0400

    initial commit

---
+++ pages/docs.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Docs() {
+    return (
+        <>
+        Docs
+        </>
+    )
+    }
---
+++ pages/team.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Team() {
+    return (
+        <>
+        Team
+        </>
+    )
+    }
